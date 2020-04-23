ac36f30d65a8b839003fbecdfd26ddb2c013536b
Eric Ma <eric.ma@block.one>
2020-04-23 11:23:44 +0800

    newaccount statistics: be more tolerance for unexpected cases

--- contracts/eosio.system/src/eosio.system.cpp
+++ contracts/eosio.system/src/eosio.system.cpp
@@ -362,25 +362,32 @@ namespace eosiosystem {
 
       new_accounts_counter_meta_table accountcntrm( get_self(), 0 );
       auto meta_it = accountcntrm.find(0);
-      // only after the meta table is initialized, start to update it
-      check(meta_it != accountcntrm.end(), "newaccount: accountcntrm not initialized");
 
-      // meta_it->accumulated_accounts_count++;
-      accountcntrm.modify(meta_it, get_self(), [&](auto& row) {
-         row.accumulated_accounts_count = row.accumulated_accounts_count + 1;
-      });
+      // the new_accounts_counter_bookkeep() function should ensure the row
+      // exists already. But check again here to be more tolerance to avoid
+      // aborting the whole transaction.
+      if (meta_it != accountcntrm.end()) {
+         // meta_it->accumulated_accounts_count++;
+         accountcntrm.modify(meta_it, get_self(), [&](auto& row) {
+            row.accumulated_accounts_count = row.accumulated_accounts_count + 1;
+         });
+      }
 
       auto cur_epoch = current_time_point().sec_since_epoch();
       auto cur_start_time = cur_epoch / STATISTICS_NEW_ACCOUNTS_INTERVAL * STATISTICS_NEW_ACCOUNTS_INTERVAL;
 
       new_accounts_counter_table accountcntr(get_self(), cur_start_time);
       auto counter_it = accountcntr.find(cur_start_time);
-      check(counter_it != accountcntr.end(), "newaccount: New accounts statistical counters corrupted");
 
-      // counter_it->count++;
-      accountcntr.modify(counter_it, get_self(), [&](auto& row) {
-         row.count = counter_it->count + 1;
-      });
+      // the new_accounts_counter_bookkeep() function should ensure the row
+      // exists already. But check again here to be more tolerance to avoid
+      // aborting the whole transaction.
+      if (counter_it != accountcntr.end()) {
+         // counter_it->count++;
+         accountcntr.modify(counter_it, get_self(), [&](auto& row) {
+            row.count = counter_it->count + 1;
+         });
+      }
    }
 
    void native::setabi( const name& acnt, const std::vector<char>& abi ) {
