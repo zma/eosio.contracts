58897710111a794e92f0e0ec598d2d65953834f1
Eric Ma <eric.ma@block.one>
2020-04-29 11:21:42 +0800

    Use a separated file for accounts related functions

--- contracts/eosio.system/src/native.cpp
+++ contracts/eosio.system/src/native.cpp
@@ -2,32 +2,7 @@
 
 #include <eosio/check.hpp>
 
-#include "abieos/abieos_numeric.hpp"
-
 namespace eosiosystem {
-   std::string format_public_key(const eosio::public_key key) {
-      abieos::public_key pkey;
-      pkey.type = abieos::key_type::k1;
-
-      auto idx = key.index();
-      if (idx == 0) {
-         auto thekey = std::get<0>(key);
-         for (int i = 0; i < thekey.size(); ++i) {
-            pkey.data[i] = thekey[i];
-         }
-      } else if (idx == 1) {
-         auto thekey = std::get<1>(key);
-         for (int i = 0; i < thekey.size(); ++i) {
-            pkey.data[i] = thekey[i];
-         }
-      } else {
-         // not supported yet
-         return "";
-      }
-
-      return abieos::public_key_to_string(pkey);
-   }
-
    void native::onerror( ignore<uint128_t>, ignore<std::vector<char>> ) {
       eosio::check( false, "the onerror action cannot be called directly" );
    }
